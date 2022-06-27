cae17b343dc48b12bf6b3f52d2ac3b6e6c90b005
aname <47141166+as06zx@users.noreply.github.com>
2022-06-27 14:45:47 +0900

    Changed if cmd == "" to cmds[cmd]()

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ import re
 summonerID = ''
 roomID = ''
 memberList = {}
-commands = []
+commands = {}
+lastConnection = None
+lastEvent = None
 
 async def sendMessage(connection, text):
     global summonerID, roomID
@@ -42,39 +44,62 @@ async def updateSummonerInfo(connection):
     summoner = (await (await connection.request('get', '/lol-summoner/v1/current-summoner')).json())
     summonerID = summoner["summonerId"]
 
+async def cmdHelp(parameter):
+    connection = lastConnection
+    await sendMessage(connection, "reply-bot\n/hi /time /membercount")
+
+async def cmdHi(parameter):
+    connection = lastConnection
+    lastMessage = lastEvent.data
+    await sendMessage(connection, memberList[lastMessage["fromSummonerId"]] + " hi!")
+
+async def cmdTime(parameter):
+    connection = lastConnection
+    tm = time.localtime(time.time())
+    year = str(tm.tm_year)
+    mon  = str(tm.tm_mon)
+    mday = str(tm.tm_mday)
+    hour = str(tm.tm_hour)
+    min  = str(tm.tm_min)
+    outMsg = f"{year}-{mon}-{mday} | {hour}:{min}"
+    await sendMessage(connection, outMsg)
+
+async def cmdMemCount(parameter):
+    connection = lastConnection
+    memberCount = await getMemberCount(connection)
+    await sendMessage(connection, "there is " + str(memberCount) + " players")
+
+async def updateCommand():
+    commands["help"] = cmdHelp
+    commands["hi"]   = cmdHi
+    commands["time"] = cmdTime
+    commands["membercount"] = cmdMemCount
+
 @connector.ready
 async def connect(connection):
     await updateSummonerInfo(connection)
     await updateRoomInfo(connection)
     await updateMemberList(connection)
+    await updateCommand()
     await sendMessage(connection, "type /help for a list of commands.")
 
 @connector.ws.register('/lol-chat/v1/conversations/', event_types=('CREATE',))
 async def onChatChanged(connection, event):
+    global lastConnection, lastEvent
     lastMessage = event.data
     body = lastMessage["body"]
     type = lastMessage["type"]
+    lastConnection = connection
+    lastEvent = event
 
     if type != "groupchat":
         return
 
-    if lastMessage["body"] == "/help":
-        await sendMessage(connection, "reply-bot\n/hi /time /membercount")
-    if lastMessage["body"] == "/hi":
-        await sendMessage(connection, memberList[lastMessage["fromSummonerId"]] + " hi!")
-    if lastMessage["body"] == "/time":
-        tm = time.localtime(time.time())
-        year = str(tm.tm_year)
-        mon  = str(tm.tm_mon)
-        mday = str(tm.tm_mday)
-        hour = str(tm.tm_hour)
-        min  = str(tm.tm_min)
-        outMsg = f"{year}-{mon}-{mday} | {hour}:{min}"
-        await sendMessage(connection, outMsg)
-    if lastMessage["body"] == "/membercount":
-        memberCount = await getMemberCount(connection)
-        await sendMessage(connection, "there is " + str(memberCount) + " players")
-
+    if body[0:1] == "/":
+        command = (body[1:]).split(" ", 1)[0]
+        parameters = re.split('\s+', body[len(command)+1:len(body)].strip())
+        if command in commands:
+            await commands[command](parameters)
 
 @connector.close
 async def disconnect(connection):
