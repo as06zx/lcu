3c7dbf14f00160f52cbfe2713179e7dd1c8698dd
aname <47141166+as06zx@users.noreply.github.com>
2022-06-28 14:45:02 +0900

    Added chat delay for no spammy
    ok

--- chat.py
+++ chat.py
@@ -1,9 +1,23 @@
+import time
+
 import room
 
+lastTimeSent = 0
+delay = 2 # for prevent spam, i make delay for it.
+
 async def sendMessage(connection, text):
-    print(room.roomID)
+    global lastTimeSent
+    if not (await checkDelay()):
+        return
+    lastTimeSent = time.time()
     messageDataBody = {
     "body": "/나 " + text,
     }
     await connection.request('post', '/lol-chat/v1/conversations/' + room.roomID + '/messages', data=messageDataBody)
 
+async def checkDelay():
+    tm = time.time()
+    if tm-delay > lastTimeSent:
+        return True
+    else:
+        return False
